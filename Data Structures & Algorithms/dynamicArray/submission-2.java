70e4648c20b819396e1d64504ca3fc8834a4617e
Abhijit Kedare <3063306+kedare01@users.noreply.github.com>
2026-04-20 23:04:33 +0530

    Add: dynamicArray - submission-2

---
+++ Data Structures & Algorithms/dynamicArray/submission-2.java
@@ -0,0 +1,49 @@
+class DynamicArray {
+
+    int capacity;
+    int length;
+    int[] arr;
+
+    public DynamicArray(int capacity) {
+        this.capacity = capacity;
+        this.length = 0;
+        this.arr = new int[capacity];
+    }
+
+    public int get(int i) {
+        return arr[i];
+    }
+
+    public void set(int i, int n) {
+        if (i < 0 || i >= length) {
+            throw new IllegalArgumentException("index out of range");
+        }
+        arr[i] = n;
+    }
+
+    public void pushback(int val) {
+        if (length == capacity) {
+            resize();
+        }
+        arr[length++] = val;
+    }
+
+    public int popback() {
+        return arr[--length];
+    }
+
+    private void resize() {
+        capacity *= 2;
+        int[] newArr = new int[capacity];
+        System.arraycopy(arr, 0, newArr, 0, length);
+        arr = newArr;
+    }
+
+    public int getSize() {
+        return length;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
