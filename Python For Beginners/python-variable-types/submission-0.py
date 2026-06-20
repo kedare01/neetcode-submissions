43abd220e90f9fe2e0b1904983688025ae2e7d6c
Abhijit Kedare <3063306+kedare01@users.noreply.github.com>
2026-06-20 19:00:33 +0530

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,11 @@
+integer_type = 25
+float_type = 98.6
+bool_type = True
+str_type = "Hello"
+list_type = [1,2,3]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(str_type))
+print(type(list_type))
